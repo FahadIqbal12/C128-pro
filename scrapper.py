e08330aa0811e0f4d7d8dc35f5eaf049fb9d39ad
Fahad Iqbal Zafar <67633549+FahadIqbal12@users.noreply.github.com>
2022-01-08 16:32:34 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -4,41 +4,34 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
-START_URL = "https://exoplanets.nasa.gov/discovery/exoplanet-catalog/"
+START_URL = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
 browser = webdriver.Chrome("chromedriver")
 browser.get(START_URL)
 time.sleep(10)
-header=['NAME','LIGHT-YEARS FROM EARTH','PLANET MASS','STELLAR MAGNITUDE','DISCOVERY DATE','hyperlink','PLANET TYPE','PLANET RADIUR','ORBITAL RADIUS','ORBITAL PERIOD','ECCENTRICITY']
-planet_data = []
-new_planet_data = []
+headers=['Brown Dwarf','Distance','Mass']
+dwarf_data = []
+new_dwarf_data = []
+
 
 def scrape():
-   
-    for i in range(0,428):
-        soup = BeautifulSoup(browser.page_source,'html.parser')
-        for ul_tag in soup.find_all('ul',attrs={'class','exoplanet'}):
-            li_tags = ul_tag.find_all('li')
-            temp_list = []
-            for index,li_tag in enumerate(li_tags):
-                if index == 0:
-                    temp_list.append(li_tag.find_all('a')[0].contents[0])
-                else:
-                    try:
-                        temp_list.append(li_tag.contents[0])
-                    except:
-                        temp_list.append('')
-            
-            hyperlink_li_tag = li_tags[0]
-            temp_list.append('https://exoplanets.nasa.gov'+hyperlink_li_tag.find_all('a',href=True)[0]['href'])
-            planet_data.append(temp_list)
-        browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
-      #  print(f'{i}page done 1')
-   # with open('data.csv','w') as f:
-    #    csvwriter = csv.writer(f)
-     #   csvwriter.writerow(header)
-      #  csvwriter.writerows(planet_data)
+  
+  for i in range(0,228):
+    soup = BeautifulSoup(browser.page_source,"html.parser")
+    for tr_tag in soup.find_all("tr"):
+      td_tags = tr_tag.find_all("td")
+      temp_list = []
+      for index, td_tag in enumerate("td_tags"):
+        if index == 0:
+          temp_list.append(td_tag.find_all("a")[0].contents[0])
+        else:
+          try:
+            temp_list.append(td_tag.contents[0])
+          except:
+            temp_list.append("")
+       dwarf_data.append(temp_list)
 
-#scrape()
+
+
 def scrape_more_data(hyperlink):
     page = requests.get(hyperlink)
     soup = BeautifulSoup(page.content,'html.parser')
@@ -54,25 +47,15 @@ def scrape_more_data(hyperlink):
         new_planet_data.append(temp_list)
 
 scrape()
-for data in planet_data:
+for data in dwarf_data:
     scrape_more_data(data[5])
 
-final_planet_data = []
-for index, data in enumerate(planet_data):
-    final_planet_data.append(data + final_planet_data[index])
+final_dwarf_data = []
+for index, data in enumerate(dwarf_data):
+    final_data.append(data + final_planet_data[index])
 
 
 with open('data.csv','w') as f:
     csvwriter = csv.writer(f)
     csvwriter.writerow(header)
-    csvwriter.writerows(final_planet_data)
-
-
-
-
-
-
-
-
-    
-
+    csvwriter.writerows(final_data)
